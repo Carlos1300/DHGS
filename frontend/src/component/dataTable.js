9ce99b79d3d8ffbf6aa228d8c3cfa84dfce3ac4a
Carlos Avila <a01750220@tec.mx>
2022-11-29 11:24:14 -0600

    deleted:    frontend/src/App.test.js 	modified:   frontend/src/component/dataTable.js 	modified:   frontend/src/index.js 	deleted:    frontend/src/logo.svg 	deleted:    frontend/src/reportWebVitals.js 	deleted:    frontend/src/setupTests.js

--- frontend/src/component/dataTable.js
+++ frontend/src/component/dataTable.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../general.scss";
-import { DataGrid} from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
 import { Link } from "react-router-dom";
 
 const columns = [
